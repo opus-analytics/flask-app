3de60d134d226cede62677b09b06179de1299469
Eman-Hamied-Ali <eman.abdelhamied@rightfoot.org>
2023-10-05 20:53:30 +0300

    Opus

--- app.py
+++ app.py
@@ -11,19 +11,22 @@ from flask import Flask, request
 from werkzeug.security import generate_password_hash, check_password_hash
 import mysql.connector
 from mysql.connector import Error
-from itsdangerous import URLSafeTimedSerializer
-
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
+from smtplib import SMTP
+import ssl 
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'test123'
 app.config['UPLOAD_FOLDER'] = 'static/files'
-app.config['MAIL_SERVER'] = 'smtp-mail.outlook.com'
+app.config['MAIL_SERVER'] = 'smtp.office365.com'
+
 app.config['MAIL_PORT'] = 587
-app.config['MAIL_USERNAME'] = 'info@opusanalytics.ai'
-app.config['MAIL_PASSWORD'] = 'YhnvfR^1998'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'eman.abdelhamied@rightfoot.org'
+app.config['MAIL_PASSWORD'] = 'Em@281194'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
 mail = Mail(app)
 
 s = URLSafeTimedSerializer(app.config['SECRET_KEY'])
@@ -112,6 +115,7 @@ def sign_in():
 @app.route("/sign-up", methods = ["GET","POST"])
 
 def sign_up():
+    token = ''
     connection = mysql.connector.connect(host='opus-server.mysql.database.azure.com',database='opus_prod',user='opusadmin',password='OAg@1234')
     cursor = connection.cursor()
     full_name = request.form.get("Full-Name")
@@ -128,27 +132,40 @@ def sign_up():
         if count > 0:
             flash('email already exists!','error')
         else:
-            hashed_pwd = generate_password_hash(password)
-            insertion_query = f"INSERT INTO users (username, password, email, full_name, company_name,verification_status) VALUES ('{email}','{hashed_pwd}','{email}', '{full_name}', '{company}', 'Pending')"
+            #hashed_pwd = generate_password_hash(password)
+            insertion_query = f"INSERT INTO users (username, password, email, full_name, company_name,verification_status) VALUES ('{email}','{password}','{email}', '{full_name}', '{company}', 'Pending')"
             cursor.execute(insertion_query)
             connection.commit()
+            connection.close()
             token = s.dumps(email, salt= 'email-confirm')
-            #msg = Message('Confirm Email', sender='info@opusanalytics.ai', recipients=[email])
-            #link = url_for('confirm_email',token=token, external=True)
+            msg = Message('Confirm Email', sender='eman.abdelhamied@rightfoot.org', recipients=[email])
+            link = url_for('confirmed_mail',token=token, _external=True)
+            msg.body = 'Your Confirmation link is {}'.format(link)
+            mail.send(msg)
             return (redirect(url_for('confirmation_mail')))
 
-    return (render_template("signup.html"))
+    return (render_template("signup.html",token = token))
 
 
 @app.route("/confirmation-mail")
 def confirmation_mail():
-
+    
     return (render_template('confirmation-mail.html'))
 
 
-@app.route("/confirmed-mail")
-def confirmed_mail():
-    
+@app.route("/confirmed-mail/<token>")
+def confirmed_mail(token):
+    try:
+        email = s.loads(token, salt='email-confirm', max_age=3600)
+        connection = mysql.connector.connect(host='opus-server.mysql.database.azure.com',database='opus_prod',user='opusadmin',password='OAg@1234')
+        cursor = connection.cursor()
+        check_mail_query = f"UPDATE USERS SET verification_status = 'Verified' WHERE email = '{email}'"
+        cursor.execute(check_mail_query)
+        connection.commit()
+        connection.close()
+
+    except SignatureExpired:
+        return (render_template('404.html'))
     return (render_template('confirm-email.html'))
 
 @app.route("/reset-password")
