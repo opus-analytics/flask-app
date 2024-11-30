b584b7aeb341da95119f4783715e686fccd985e6
OmarrAlaa17 <102559077+OmarrAlaa17@users.noreply.github.com>
2024-11-30 20:51:36 +0200

    Update email password

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ app.config['MAIL_SERVER'] = 'smtp.office365.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = 'CustomerExperience@opusanalytics.ai'
 
-app.config['MAIL_PASSWORD'] = 'Mog38103'
+app.config['MAIL_PASSWORD'] = 'R*145568865662uY'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 powerbi_blueprint = Blueprint('powerbi', __name__)
